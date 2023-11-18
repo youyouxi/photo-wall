34c384ba372880ac202d4afc7f9f003cacf42fc0
陈建斌 <chenjb1287@fingard.com>
2023-11-18 16:38:05 +0800

    init gitignore

---
+++ src/main/java/com/youyouxi/photo/wall/PhotoWallApplication.java
@@ -0,0 +1,13 @@
+package com.youyouxi.photo.wall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoWallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youyouxi/photo/wall/PhotoWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youyouxi.photo.wall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoWallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
